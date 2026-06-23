0673bbf8e3b114bfad84536e32e161cb2e18029f
X Abdullah <x@Xs-MacBook-Pro.local>
2026-06-23 15:55:49 +0300

    fix: switch to HashRouter for GitHub Pages SPA routing
    - BrowserRouter doesn't work on gh-pages (CSP blocks 404.html redirect)
    - HashRouter: /chat/#/agent works everywhere, no server config needed
    - Remove 404.html SPA trick (not needed with hash routing)

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import { Sidebar } from "@/components/layout/Sidebar";
 import { Header } from "@/components/layout/Header";
 import { ChatPage } from "@/pages/ChatPage";
@@ -9,7 +9,7 @@ import { Toaster } from "sonner";
 
 export default function App() {
     return (
-        <BrowserRouter basename="/chat">
+        <HashRouter basename="/chat">
             <div className="flex h-screen bg-white dark:bg-neutral-950 text-neutral-900 dark:text-neutral-100">
                 <Sidebar />
                 <div className="flex-1 flex flex-col min-w-0">
@@ -23,6 +23,6 @@ export default function App() {
                 </div>
             </div>
             <Toaster position="bottom-center" />
-        </BrowserRouter>
+        </HashRouter>
     );
 }
